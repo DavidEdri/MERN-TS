7567078d90df8cf65c47b65aa4bcaeeec2f5e471
DavidEdri <davidedri5392@gmail.com>
2021-03-02 15:14:25 +0200

    userToApi typing fix

--- packages/server/src/utils/functions/userToApi.ts
+++ packages/server/src/utils/functions/userToApi.ts
@@ -1,6 +1,9 @@
 import { pick } from "lodash";
 // eslint-disable-next-line import/no-cycle
 import { UserDocument } from "../../models/User";
+import { BaseUser } from "../../models/User/DocumentType";
 
-export const userToApi = (user: UserDocument) =>
+type User = UserDocument | BaseUser;
+
+export const userToApi = (user: User) =>
   pick(user, ["_id", "name", "email", "rank", "active"]);
